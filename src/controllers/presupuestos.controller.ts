6c807f1329731f2252ddaecbd40bfab9554ee1c1
universalanalisismant-stack <marcosalfredoaguilarbeleno@gmail.com>
2026-06-19 12:45:44 -0500

    update

--- src/controllers/presupuestos.controller.ts
+++ src/controllers/presupuestos.controller.ts
@@ -746,12 +746,20 @@ export const presupuestosController = {
                         const end = new Date(Date.UTC(yearNum, m, 0, 23, 59, 59, 999));
                         return getDateQuery(start, end);
                     });
-                    mongoQuery.$and = [{ $or: ranges }];
+                    if (mongoQuery.$and) {
+                        mongoQuery.$and.push({ $or: ranges });
+                    } else {
+                        mongoQuery.$and = [{ $or: ranges }];
+                    }
                 } else {
                     const start = new Date(Date.UTC(yearNum, 0, 1, 0, 0, 0, 0));
                     const end = new Date(Date.UTC(yearNum, 11, 31, 23, 59, 59, 999));
                     const baseQuery = getDateQuery(start, end);
-                    mongoQuery.$and = [{ $or: baseQuery.$or }];
+                    if (mongoQuery.$and) {
+                        mongoQuery.$and.push({ $or: baseQuery.$or });
+                    } else {
+                        mongoQuery.$and = [{ $or: baseQuery.$or }];
+                    }
                 }
 
                 const aggregateResult = await PagoModel.aggregate([
